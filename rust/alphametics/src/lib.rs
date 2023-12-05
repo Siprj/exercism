3aa979fb34189a8431ccba183d5865566fbbc54f
Jan Sipr <sipr.jan@gmail.com>
2023-12-05 11:37:14 +0100

    Improving the test with weights to speed up the sum computation

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,7 +1,6 @@
 use std::{
-    collections::{BTreeMap, BTreeSet, HashMap},
+    collections::{BTreeSet, HashMap},
     iter::once,
-    ops::RangeInclusive,
 };
 
 use itertools::Itertools;
@@ -13,62 +12,61 @@ fn parse(input: &str) -> (Vec<&str>, &str) {
     (left, right)
 }
 
-fn map_string(s: &str, map: &BTreeMap<char, char>) -> u64 {
-    let string: String = s.to_string();
-    let mut new_string = String::new();
-    for c in string.chars() {
-        if let Some(d) = map.get(&c) {
-            new_string.push(*d);
+fn compute_weights(left: &[&str], right: &str) -> HashMap<char, i64> {
+    let mut weights: HashMap<char, i64> = HashMap::new();
+    for w in left {
+        for (i, c) in w.chars().rev().enumerate() {
+            weights
+                .entry(c)
+                .and_modify(|v| *v += 10i64.pow(i as u32))
+                .or_insert(10i64.pow(i as u32));
         }
     }
-    new_string.parse().unwrap()
+    for (i, c) in right.chars().rev().enumerate() {
+        weights
+            .entry(c)
+            .and_modify(|v| *v -= 10i64.pow(i as u32))
+            .or_insert(-(10i64.pow(i as u32)));
+    }
+    weights
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let (left, right) = parse(input);
+    let weights = compute_weights(&left, right);
     let first_letters: BTreeSet<char> = left
         .iter()
         .map(|l| l.chars().next().unwrap())
         .chain(once(right.chars().next().unwrap()))
         .collect();
-    let unique_letters: BTreeSet<char> = left
-        .iter()
-        .flat_map(|l| l.chars())
-        .chain(right.chars())
-        .collect();
-    let unique_letters: Vec<&char> = unique_letters.iter().collect();
-    let first_letters: BTreeSet<usize> = first_letters
-        .iter()
-        .map(|c1| unique_letters.iter().position(|c2| *c1 == **c2).unwrap())
-        .collect();
 
-    if unique_letters.len() > 10 {
+    let len = weights.keys().len();
+    if len > 10 {
         return None;
     }
 
-    let range: RangeInclusive<char> = '0'..='9';
-
-    let mut iteration_count = 0;
-    for values in range.permutations(unique_letters.len()) {
-        iteration_count += 1;
+    for values in (0u8..10).permutations(len) {
         if values
             .iter()
-            .enumerate()
-            .any(|(i, c)| c == &'0' && first_letters.contains(&i))
+            .zip(weights.keys())
+            .any(|(n, c)| n == &0 && first_letters.contains(c))
         {
             continue;
         }
 
-        let map: BTreeMap<char, char> = unique_letters
+        let sum: i64 = values
             .iter()
-            .copied()
-            .copied()
-            .zip(values)
-            .collect();
-        let left: u64 = left.iter().map(|s| map_string(s, &map)).sum();
-        let right: u64 = map_string(right, &map);
-        if left == right {
-            return Some(map.iter().map(|(&k, &v)| (k,v.to_digit(10).unwrap() as u8)).collect());
+            .zip(weights.values())
+            .map(|(n, w)| (*n as i64) * w)
+            .sum();
+        if sum == 0 {
+            return Some(
+                weights
+                    .iter()
+                    .zip(values.iter())
+                    .map(|((k, _), v)| (*k, *v))
+                    .collect(),
+            );
         }
     }
     None
