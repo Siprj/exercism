2a081260f112804bae4aef1402f482cdfd401604
Jan Sipr <sipr.jan@gmail.com>
2023-12-07 17:20:51 +0100

    Ok, this was stupidly simple

---
+++ rust/etl/src/lib.rs
@@ -0,0 +1,5 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    h.iter().flat_map(|(points, letters)| letters.iter().map(|c| (c.to_ascii_lowercase(), *points))).collect()
+}
